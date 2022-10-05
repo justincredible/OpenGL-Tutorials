02b740089aae5fddb679098b3515a7d41c348d19
Justin Symonds <justinjfsymonds@hotmail.com>
2022-10-05 00:57:42 -0700

    Removed redundancy.

--- Flow/Render.hs
+++
@@ -1,7 +0,0 @@
-module Flow.Render where
-
-class Render a where
-    render :: a -> IO (Bool,a)
-    
-instance Render a => Render (Maybe a) where
-    render = maybe (pure (True,Nothing)) (fmap (fmap Just) . render)
